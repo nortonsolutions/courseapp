1876cc891d8aa1f5a16d8c991574f56e89de7a15
David Norton <norton@whatever.com>
2021-11-23 15:17:17 -0700

    Smalll profile fix for role updates

--- viewScripts/profileScripts.js
+++ viewScripts/profileScripts.js
@@ -1,20 +1,27 @@
+var admin = document.getElementById('admin').innerText=="true";
+
 document.getElementById('userUpdateForm').addEventListener('submit', (e) => {
     
-    // Get the roles
-    var rolesArray = [];
-    Array.from(document.querySelectorAll('#rolesSelect option')).forEach(option => {
-        if (option.selected) rolesArray.push(option.value);
-    })
-    
-    handlePost('/updateAccount', {
+    let updateAccountObj = {
         _id: e.target.elements.id.value,
         username: e.target.elements.username.value,
         firstname: e.target.elements.firstname.value,
         surname: e.target.elements.surname.value,
         password: e.target.elements.password.value,
         confirm: e.target.elements.confirm.value,
-        roles: rolesArray
-    }, (response) => {
+    }
+
+    if (admin) {
+        // Get the roles
+        var rolesArray = [];
+        Array.from(document.querySelectorAll('#rolesSelect option')).forEach(option => {
+            if (option.selected) rolesArray.push(option.value);
+        })
+
+        updateAccountObj.roles = rolesArray;
+    }
+
+    handlePost('/updateAccount', updateAccountObj, (response) => {
         document.getElementById('feedback').innerHTML = response;
     })
     e.preventDefault();
