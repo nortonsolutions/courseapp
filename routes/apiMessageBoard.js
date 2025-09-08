507da0bcb10b88a52ec5f1d93a5fff3d01ebf645
Dave Norton <davidandrewnorton@gmail.com>
2025-09-08 13:28:56 -0600

    setCurrentTeacher middleware

--- routes/apiMessageBoard.js
+++ routes/apiMessageBoard.js
@@ -59,6 +59,11 @@ module.exports = function(app,db) {
 
   // checkIfTeacher
   const setCurrentTeacher = (req, res, next) => {
+
+      if (!req.user || !req.user.roles) {
+          req.user.currentTeacher = false;
+          next();
+      }
       if (req.user.roles.includes('teacher')) {
           let courseId = req.params.courseId;
           
